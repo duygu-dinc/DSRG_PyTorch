24a63d388ad7a4ab7448850dc6dd9945d95b5e8d
evren-cilden <evren-cilden@users.noreply.github.com>
2021-06-06 17:03:00 +0300

    DSRG_Pytorch dizin eklemeleri yapildi

--- test_multiprocess.sh
+++ test_multiprocess.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 arch=deeplab_large_fov
-image_list='./datalist/PascalVOC/val_id.txt'
-image_path='./dataset/PascalVOC/VOCdevkit/VOC2012'
-cls_labels_path='./datalist/PascalVOC/cls_labels.npy'
-log_path='./train_log/1'
-pred_path='./result/1'
+image_list='./DSRG_PyTorch/datalist/PascalVOC/val_id.txt'
+image_path='./DSRG_PyTorch/dataset/PascalVOC/VOCdevkit/VOC2012'
+cls_labels_path='./DSRG_PyTorch/datalist/PascalVOC/cls_labels.npy'
+log_path='./DSRG_PyTorch/train_log/1'
+pred_path='./DSRG_PyTorch/result/1'
 trained=${log_path}/last_checkpoint.pth.tar
 smooth=True
 color_mask=1
