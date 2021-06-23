bffbc0fecbaed53bbbe7dfa5d61d3bed0f3c9b87
evren-cilden <evren-cilden@users.noreply.github.com>
2021-06-23 22:43:38 +0300

    Dizinler duzenlendi, color mask e gore evaluation yapilmasi icin ayarlar guncellendi

--- evaluation.sh
+++ evaluation.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-image_list='./datalist/PascalVOC/val_id.txt'
-gt_path="./dataset/PascalVOC/VOCdevkit/VOC2012/SegmentationClassAug/"
-result_path='./result/1'
-pred_path=${result_path}/label_mask
+image_list='./DSRG_PyTorch/datalist/PascalVOC/val_id.txt'
+gt_path="./DSRG_PyTorch/dataset/VOC2012/SegmentationClassAug/"
+result_path='./DSRG_PyTorch/result/1'
+#pred_path=${result_path}/label_mask
+pred_path=${result_path}/pred
 save_name=${result_path}/evaluation_result_direct.txt
-color_mask=0
+#color_mask=0
+color_mask=1
 
-python evaluation.py \
+python ./DSRG_PyTorch/evaluation.py \
   --image-list ${image_list} \
   --pred-path ${pred_path} \
   --gt-path ${gt_path} \
