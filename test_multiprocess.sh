85f39ac0307eb32ef1ed05bb9771f65c5f1d8bb0
evren-cilden <evren-cilden@users.noreply.github.com>
2021-06-23 21:54:09 +0300

    Train edilmis modeli kullanmasi saglandi. Split size colab ortami nedeniyle azaltildi. Dizin ayarlandi.

--- test_multiprocess.sh
+++ test_multiprocess.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 arch=deeplab_large_fov
 image_list='./DSRG_PyTorch/datalist/PascalVOC/val_id.txt'
-image_path='./DSRG_PyTorch/dataset/PascalVOC/VOCdevkit/VOC2012'
+image_path='./DSRG_PyTorch/dataset//VOC2012'
 cls_labels_path='./DSRG_PyTorch/datalist/PascalVOC/cls_labels.npy'
-log_path='./DSRG_PyTorch/train_log/1'
+log_path='./train_log/1'
 pred_path='./DSRG_PyTorch/result/1'
 trained=${log_path}/last_checkpoint.pth.tar
 smooth=True
@@ -19,6 +19,6 @@ python3 ./DSRG_PyTorch/test_multiprocess.py \
   --cls-labels-path ${cls_labels_path} \
   --gpu ${gpu} \
   --num-gpu 1 \
-  --split-size 8 \
+  --split-size 1 \
   --smooth \
   --color-mask ${color_mask} \
